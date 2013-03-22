ccb3cbda09f7f069135f8630d5dff6815c0dfdcc
Dan Kogai <dankogai+github@gmail.com>
2013-03-22 11:28:53 +0900

    0th commit

---
+++ test/wrap.js
@@ -0,0 +1,15 @@
+/*
+ * use mocha to test me
+ * http://visionmedia.github.com/mocha/
+ */
+if (this['window'] !== this) {
+    require('./helper.js');
+    require('../wrap.js');
+}
+
+(function(root){
+    'use strict';
+    describe('OK', function() {
+        it ('is ok', ok(true));
+    });
+})(this);
